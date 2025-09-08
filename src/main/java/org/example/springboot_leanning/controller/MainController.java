db93d639921504d481785277358990e8bc382589
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-08 16:36:37 +0700

    add logging to home request in MainController

--- src/main/java/org/example/springboot_leanning/controller/MainController.java
+++ src/main/java/org/example/springboot_leanning/controller/MainController.java
@@ -1,12 +1,17 @@
 package org.example.springboot_leanning.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+    private Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping("/home")
     public String home() {
+        logger.info("request processing!!!");
         return "home.html";
     }
 }
