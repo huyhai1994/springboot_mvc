d55a15efd52a3515eafe5285dd51c09be77bb5c8
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-08 15:28:20 +0700

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/test/java/org/example/springboot_leanning/SpringbootLeanningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot_leanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLeanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
