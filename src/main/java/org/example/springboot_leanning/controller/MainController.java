1744a45903d40e83f7ae020f5f8b1029d8b6b542
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-11 06:09:30 +0700

    implementing web apps with a dynamic view

--- src/main/java/org/example/springboot_leanning/controller/MainController.java
+++ src/main/java/org/example/springboot_leanning/controller/MainController.java
@@ -3,6 +3,7 @@ package org.example.springboot_leanning.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +11,10 @@ public class MainController {
     private Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model page) {
         logger.info("request processing!!!");
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", "red");
         return "home.html";
     }
 }
