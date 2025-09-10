4d86dd48ea0a3d4b648475ea0ffb94eb801f36e2
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-11 06:26:28 +0700

    getting a value through a request parameter.

--- src/main/java/org/example/springboot_leanning/controller/MainController.java
+++ src/main/java/org/example/springboot_leanning/controller/MainController.java
@@ -5,16 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     private Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping("/home")
-    public String home(Model page) {
+    public String home(@RequestParam String color, @RequestParam String name, Model page) {
         logger.info("request processing!!!");
-        page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
