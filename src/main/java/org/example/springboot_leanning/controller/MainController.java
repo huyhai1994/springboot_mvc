71c70c80eae18a2e384179b29ce7d4be52999f71
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-11 06:28:16 +0700

    Using path variables to get values from the client

--- src/main/java/org/example/springboot_leanning/controller/MainController.java
+++ src/main/java/org/example/springboot_leanning/controller/MainController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
     private Logger logger = LoggerFactory.getLogger(MainController.class);
 
-    @RequestMapping("/home")
-    public String home(@RequestParam String color, @RequestParam String name, Model page) {
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color, Model page) {
         logger.info("request processing!!!");
-        page.addAttribute("username", name);
+        page.addAttribute("username", "Hai");
         page.addAttribute("color", color);
         return "home.html";
     }
