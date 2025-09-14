3e57d59c761f3f321ad1dc069a3248c4aa13068a
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-15 06:32:16 +0700

    refactor: update home method to use Optional for color path variable

--- src/main/java/org/example/springboot_leanning/controller/MainController.java
+++ src/main/java/org/example/springboot_leanning/controller/MainController.java
@@ -8,15 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @Slf4j
 public class MainController {
 
     @RequestMapping("/home/{color}/{username}")
     @ValidateInput
-    public String home(@PathVariable @NonNull String color, @PathVariable String username, Model page) {
+    public String home(@PathVariable @NonNull Optional<String> color, @PathVariable String username, Model page) {
         page.addAttribute("username", username);
-        page.addAttribute("color", color);
+        page.addAttribute("color", color.orElse("black"));
         return "home.html";
     }
 }
