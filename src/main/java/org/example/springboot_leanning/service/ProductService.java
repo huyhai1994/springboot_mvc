4fe2eccc1c7e2ad686c93dab3c90dca0bb3c4784
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-17 06:23:23 +0700

    Listing 8.6 The ProductService class implements the app’s use cases and Listing 8.5 The Product class describes a product with name and price as attributes

---
+++ src/main/java/org/example/springboot_leanning/service/ProductService.java
@@ -0,0 +1,21 @@
+package org.example.springboot_leanning.service;
+
+import org.example.springboot_leanning.entity.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p) {
+        products.add(p);
+    }
+
+    public List<Product> findAll() {
+        return products;
+    }
+
+}
