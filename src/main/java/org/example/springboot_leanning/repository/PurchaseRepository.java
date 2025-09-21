c74eae70e5739c45e668a28aa2382de1130733b4
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-22 06:04:09 +0700

    Listing 12.2 Injecting a JdbcTemplate bean to work with persisted data.

---
+++ src/main/java/org/example/springboot_leanning/repository/PurchaseRepository.java
@@ -0,0 +1,14 @@
+package org.example.springboot_leanning.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PurchaseRepository {
+    private final JdbcTemplate jdbc;
+
+    public PurchaseRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+}
