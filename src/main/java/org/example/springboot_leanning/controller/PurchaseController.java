5e2525de1fd664c0e942339c1c004e02b83a8f9a
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-22 06:28:32 +0700

    fix the insert query

---
+++ src/main/java/org/example/springboot_leanning/controller/PurchaseController.java
@@ -0,0 +1,23 @@
+package org.example.springboot_leanning.controller;
+
+import org.example.springboot_leanning.entity.Purchase;
+import org.example.springboot_leanning.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+}
